223a7dee87f64f1194cc3fe8f924ed62c0461817
Esese Joshua <esesejoshua01@gmail.com>
2024-01-11 11:12:55 +0100

    Blog API setup

---
+++ posts/views.py
@@ -0,0 +1,13 @@
+from rest_framework import generics
+from .models import Post
+from .serializers import PostSerializer
+
+# Create your views here.
+
+class PostList(generics.ListCreateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+
+class PostDetail(generics.RetrieveUpdateAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
